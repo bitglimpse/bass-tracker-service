e2c00d79af6d67a4e2c4e0ca197962fbce5ed9e7
bitglimpse <bitglimpse@gmail.com>
2018-11-27 22:39:17 -0700

    Initial commit

---
+++ src/main/java/com/saasbass/basstrackerservice/BassTrackerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.saasbass.basstrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BassTrackerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BassTrackerServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/saasbass/basstrackerservice/BassTrackerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saasbass.basstrackerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BassTrackerServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
