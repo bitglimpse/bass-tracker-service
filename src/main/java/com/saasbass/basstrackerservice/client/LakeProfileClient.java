2530e30d91d0faa19f66941ecf4d4191ead1f22e
Tyler <tyler@yourmom.com>
2019-02-23 09:45:22 -0700

    cleanup

--- src/main/java/com/saasbass/basstrackerservice/client/LakeProfileClient.java
+++ src/main/java/com/saasbass/basstrackerservice/client/LakeProfileClient.java
@@ -1,15 +1,11 @@
 package com.saasbass.basstrackerservice.client;
 
-import com.saasbass.basstrackerservice.BassTrackerServiceApplication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class LakeProfileClient {
     private RestTemplate restTemplate;
     private String lakeProfileServiceBaseUrl;
-    private static final Logger log = LoggerFactory.getLogger(BassTrackerServiceApplication.class);
 
     public LakeProfileClient(HttpComponentsClientHttpRequestFactory clientFactory,
                              String lakeProfileServiceBaseUrl) {
@@ -19,13 +15,11 @@ public class LakeProfileClient {
 
     public LakeProfile getLakeProfile(Long id) {
         String url = lakeProfileServiceBaseUrl + "/lake-profile/" + id;
-        log.info("--- Get lake profile attempt ---");
         return restTemplate.getForObject(url, LakeProfile.class);
     }
 
     public void createLakeProfile(LakeProfile lakeProfile) {
         String url = lakeProfileServiceBaseUrl + "/lake-profile";
-        log.info("--- Create lake profile attempt ---");
         restTemplate.postForObject(url, lakeProfile, LakeProfile.class);
     }
 }
