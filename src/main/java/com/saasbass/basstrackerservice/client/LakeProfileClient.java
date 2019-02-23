f99f8a757432d5139d07f1cf58a098e9442c62c7
Tyler <tyler@yourmom.com>
2019-02-23 09:38:11 -0700

    update lakeprofileclient

--- src/main/java/com/saasbass/basstrackerservice/client/LakeProfileClient.java
+++ src/main/java/com/saasbass/basstrackerservice/client/LakeProfileClient.java
@@ -1,11 +1,15 @@
 package com.saasbass.basstrackerservice.client;
 
+import com.saasbass.basstrackerservice.BassTrackerServiceApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class LakeProfileClient {
     private RestTemplate restTemplate;
     private String lakeProfileServiceBaseUrl;
+    private static final Logger log = LoggerFactory.getLogger(BassTrackerServiceApplication.class);
 
     public LakeProfileClient(HttpComponentsClientHttpRequestFactory clientFactory,
                              String lakeProfileServiceBaseUrl) {
